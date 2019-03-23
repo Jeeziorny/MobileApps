efa0a17dbabdabb6b75238b4c7b682358603ae8e
kantj <kamij98@gmail.com>
2019-03-23 01:03:16 +0100

    Project done

--- TicTacToe/app/src/main/java/com/example/tictactoe/MainActivity.kt
+++ TicTacToe/app/src/main/java/com/example/tictactoe/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.tictactoe
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.Button
 import kotlinx.android.synthetic.main.activity_main.*
@@ -10,8 +9,9 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
 
     lateinit var buttons: Array<Button>
-    var board = IntArray(25)
+    var bd = IntArray(25)
     var sumOnLine = IntArray(12)
+
 
     enum class winner {
         USER, BOT, NONE, NOTYET
@@ -30,7 +30,7 @@ class MainActivity : AppCompatActivity() {
     fun userService(view: View) {
         (view as Button).text = "O"
         view.setOnClickListener(null)
-        board[buttons.indexOf(view)] = 1
+        bd[buttons.indexOf(view)] = 1
         sumUpdate()
         when (isFinished()) {
             winner.USER -> announce(winner.USER)
@@ -42,7 +42,12 @@ class MainActivity : AppCompatActivity() {
 
     fun again(view: View) {
         buttons.forEach { it.setOnClickListener { e -> userService(it) } }
-        message.text = "Let's try.."
+        buttons.forEach { it.text = ""}
+        sumOnLine.forEach { e -> 0 }
+        for (i in 0..24) {
+            bd[i] = 0
+        }
+        message.text = "Show me what you can!!!"
     }
 
     fun sumUpdate() {
@@ -50,21 +55,22 @@ class MainActivity : AppCompatActivity() {
         for(i in 0..11) sumOnLine[i] = 0
         for (j in 0..20 step 5) {
             for (i in 0..4) {
-                sumOnLine[index] += board[j + i]
+                sumOnLine[index] += bd[j + i]
             }
             index++
         }
         for (i in 0..4) {
             for (j in 0..20 step 5) {
-                sumOnLine[index] += board[j + i]
+                sumOnLine[index] += bd[j + i]
             }
             index++
         }
-        sumOnLine[index++] = board[0]+board[6]+board[12]+board[18]+board[24]
-        sumOnLine[index] = board[20]+board[16]+board[12]+board[8]+board[4]
+        sumOnLine[index++] = bd[0]+bd[6]+bd[12]+bd[18]+bd[24]
+        sumOnLine[index] = bd[20]+bd[16]+bd[12]+bd[8]+bd[4]
     }
 
     fun bot() {
+        var done = false
         var index = 0
         for (i in 1..11) {
             if (sumOnLine[i] > sumOnLine[index])
@@ -72,41 +78,51 @@ class MainActivity : AppCompatActivity() {
         }
         if (index < 5) {
             for (i in 0..4) {
-                if (board[index*5+i] == 0) {
-                    board[index*5+i] = -1
+                if (bd[index*5+i] == 0) {
+                    bd[index*5+i] = -1
                     buttons[index*5+i].text = "X"
+                    buttons[index*5+i].setOnClickListener(null)
+                    done = true
                     break
                 }
             }
         } else if (index < 10) {
             for (i in 0..20 step 5) {
-                if (board[index%5+i] == 0) {
-                    board[index%5+i] = -1
+                if (bd[index%5+i] == 0) {
+                    bd[index%5+i] = -1
                     buttons[index%5+i].text = "X"
+                    buttons[index%5+1].setOnClickListener(null)
+                    done = true
                     break
                 }
             }
         } else if (index == 10) {
             for (i in 0..24 step 6) {
-                if (board[i] == 0) {
-                    board[i] = -1
+                if (bd[i] == 0) {
+                    bd[i] = -1
                     buttons[i].text = "X"
-                    break
-                }
-            }
-        } else if (index == 11) {
-            for (i in 4..20 step 4) {
-                if (board[i] == 0) {
-                    board[i] = -1
-                    buttons[i].text = "X"
+                    buttons[i].setOnClickListener(null)
+                    done = true
                     break
                 }
             }
         } else {
-            for (i in 0..24) {
-                if(board[i] == 0) {
-                    board[i] = -1
+            for (i in 4..20 step 4) {
+                if (bd[i] == 0) {
+                    bd[i] = -1
                     buttons[i].text = "X"
+                    buttons[i].setOnClickListener(null)
+                    done = true
+                    break
+                }
+            }
+        }
+         if (!done) {
+            for (i in 0..24) {
+                if(bd[i] == 0) {
+                    bd[i] = -1
+                    buttons[i].text = "X"
+                    buttons[i].setOnClickListener(null)
                     break
                 }
             }
@@ -123,7 +139,14 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun isFinished() : winner {
-        if
+        var isEnd = true
+        for(i in bd) {
+            if (i == 0) {
+                isEnd = false
+            }
+        }
+        if (isEnd)
+            return winner.NONE
         for (i in sumOnLine) {
             when(i) {
                 5  -> return winner.USER
